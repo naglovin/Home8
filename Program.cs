e2dff5ebfd46cf3b58972ffdf6549a30e9b3d5b9
Danil <Kutfilloev.danil@yandex.ru>
2022-09-02 10:40:53 +0500

    добавлено

---
+++ Program.cs
@@ -0,0 +1,187 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int [,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int [,] array = new int [rowCount, columnCount];
+    for(int row = 0; row < rowCount; row++)
+    {
+        for(int column = 0; column < columnCount; column++)
+        {
+            array [ row, column] = new Random().Next (min, max + 1);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int [,] arr)
+{
+    for(int row = 0; row < arr.GetLength(0); row++)
+    {
+        for(int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void OrderArrayLines(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      for (int k = 0; k < arr.GetLength(1) - 1; k++)
+      {
+        if (arr[i, k] < arr[i, k + 1])
+        {
+          int temp = arr[i, k + 1];
+          arr[i, k + 1] = arr[i, k];
+          arr[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+int [,] matrix = CreateMatrix(4, 4, 0, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+OrderArrayLines(matrix);
+PrintMatrix(matrix);
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Пограмма считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int [,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int [,] array = new int [rowCount, columnCount];
+    for(int row = 0; row < rowCount; row++)
+    {
+        for(int column = 0; column < columnCount; column++)
+        {
+            array [ row, column] = new Random().Next (min, max + 1);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int [,] arr)
+{
+    for(int row = 0; row < arr.GetLength(0); row++)
+    {
+        for(int column = 0; column < arr.GetLength(1); column++)
+        {
+            Console.Write($"{arr[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void SumStringMatrix(int[,] matrix)
+{
+    int index = 0;
+    int minsum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        line += matrix[index, j] + " ";
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов равна {line}. ");
+}
+int [,] matrix = CreateMatrix(4, 4, 0, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+SumStringMatrix(matrix);
+
+// // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(output);
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5); //Для увеличения размера чисел в матрицах поменять число 5 на большее
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int size = InputInt("размерность матриц: ");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+Console.WriteLine("Матрица - А");
+PrintArray(matrixA);
+Console.WriteLine();
+Console.WriteLine("Матрица - В");
+PrintArray(matrixB);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц А*В");
+PrintArray(matrixC);
